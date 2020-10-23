523bd92baf3a94e246d9386964060b36ca17a18a
seonae <seonae@kakao.com>
2020-10-23 17:47:54 +0900

    20201023(4)

--- BBS/src/co/seonae/board/dao/MemberDao.java
+++ BBS/src/co/seonae/board/dao/MemberDao.java
@@ -13,11 +13,17 @@ public class MemberDao extends DAO {
 	private ResultSet rs; //select 결과 담을 결과셋 (전체에서 부분집합을 가져 오는것.java에서는resultset(원랜recordSet)
 	private MemberVO vo;
 	
+	
+	
 	private final String SELECT_ALL = "SELECT * FROM MEMBER"; //ctrl+shift+x = 대문자 자동변환
 	private final String SELECT = "SELECT * FROM MEMBER WHERE ID=? AND PASSWORD=?";
 	//final 로 만들어서 상수로 만듬(쿼리를 누가 바깥에서 수정하지 못하게 상수로 만듦)
 	private final String INSERT = "INSERT INTO MEMBER(ID,NAME,PASSWORD,ADDRESS,TEL,ENTERDATE)"
 														+ " VALUES(?,?,?,?,?,?)";
+	private final String UPDATE = "UPDATE MEMBER SET NAME =?, PASSWORD =?, ADDRESS=?, TEL =?"
+														+ " WHERE ID=?";
+	private final String DELETE = "DELETE FROM MEMBER WHERE ID=?";
+	
 	
 	public List<MemberVO> selectAll() { //멤버list전체를 불러오기 위한 함수
 		List<MemberVO> list = new ArrayList<MemberVO>();
@@ -90,11 +96,34 @@ public class MemberDao extends DAO {
 	
 	public int update(MemberVO vo) {//memberVO에 update 하는것
 		int n =0;
+		try {
+			psmt = conn.prepareStatement(UPDATE);
+			psmt.setString(1,vo.getName());
+			psmt.setString(2,vo.getPassword());
+			psmt.setString(3,vo.getAddress());
+			psmt.setString(4,vo.getTel());
+			psmt.setString(5,vo.getId());
+			n = psmt.executeUpdate();
+			
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}finally {
+			close();
+		}
 		return  n;
 	} 
 	
 	public int delete(MemberVO vo) {//memberVO에 delete 하는것
 		int n =0;
+		try {
+			psmt = conn.prepareStatement(DELETE);
+			psmt.setString(1,vo.getId());
+			n = psmt.executeUpdate();
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}finally {
+			close();
+		}
 		return  n;
 	}
 	
